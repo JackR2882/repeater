81fa681e48f46b30261dc32a286f02a680896ebe
Jack Rive <jack.rive2@gmail.com>
2024-07-25 15:28:19 +0100

    Moved from testing

--- repeater.py
+++ repeater.py
@@ -6,72 +6,44 @@
 import requests
 import  multiprocessing as mp
 import request_parser
-
-import time
-import os
-
-
-
-#def split():
-#    # splits the payload into chunks for processes to work on
-#    print("...")
-
-
-with open("full_request.txt", "r") as req:
-    payloads = req.read().splitlines()
-    burp_url, burp_cookies, burp_headers, burp_json = request_parser.parse(payloads)
-
-
-def request(payload):
-    print('sending html request ... ... ...')
-    session = requests.session()
-
-    res = session.post(burp_url, headers=burp_headers, cookies=burp_cookies, json=burp_json)
-
-    #print(res)
-
-
-with open("payload.txt", "r") as file_in:
-    payloads = file_in.read().splitlines()
-print(payloads)
-
-
-for payload in payloads:
-    print("Sending " + payload + " as payload.")
-    #parsed = request_parser.parse(request)
-    request(payload)
+import re
+import json
 
 
 
-#if __name__ == '__main__':
-#    mp.freeze_support()
+def run(request, payload_location):
 
-#    core_count = mp.cpu_count()
-    #print(core_count)
+    burp_url, burp_cookies, burp_headers, burp_json = request_parser.parse(request.splitlines())
 
-#    t1 = time.time()
+    def request(payload):
+        print('sending html request ... ... ...')
+        session = requests.session()
 
-#    request(1)
+        replaced_json = re.sub(r''+str(payload_location), payload, str(burp_json))
 
-    #p1 = mp.Process(target=request, args=(1,))
-    #p2 = mp.Process(target=request, args=(2,))
-    #p3 = mp.Process(target=request, args=(3,))
-    #p4 = mp.Process(target=request, args=(4,))
+        print('------------------')
+        print(burp_url)
+        print('------------------')
+        print(burp_headers)
+        print('------------------')
+        print(burp_cookies)
+        print('------------------')
+        print(replaced_json)
+        print('------------------')
 
-    #p1.start()
-    #p2.start()
-    #p3.start()
-    #p4.start()
+        # load payload as json
+        replaced_json = json.loads(replaced_json)
 
-    #p1.join()
-    #p2.join()
-    #p3.join()
-    #p4.join()
+        res = session.post(burp_url, headers=burp_headers, cookies=burp_cookies, json=replaced_json)
+        
+        print(res)
 
-#    t2 = time.time()
 
-#    print('time elapsed: ' + str(t2-t1))
+    with open("payload.txt", "r") as file_in:
+        payloads = file_in.read().splitlines()
 
-    #f = open("payload.txt", "r")
-    #print(f.read())
-    #print((f.read()[1]))
+
+    for payload in payloads:
+        print("Sending " + payload + " as payload.")
+        #parsed = request_parser.parse(request)
+        request(payload)
