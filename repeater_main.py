d023c43bd9a9d904ef7131c0da905536823826c7
Jack Rive <jack.rive2@gmail.com>
2024-08-02 11:24:27 +0100

    Changes to how, payload locations are identified, will allow payloads to be anywhere in request.

--- repeater_main.py
+++ repeater_main.py
@@ -27,23 +27,33 @@ def run_repeater():
 	repeater.run(request, payload_loc) # needs to be changed to take payload_loc instead, also need
 									   # to add conditionals to handle multiple payloads in repeater.py
 
-def func1():
+#def func1():
     
     # clear any existing items from grid
-    l2.grid_remove()
-    payload_pos1.grid_remove()
-    l2_2.grid_remove()
-    payload_pos2.grid_remove()
+#    l2.grid_remove()
+#    payload_pos1.grid_remove()
+#    l2_2.grid_remove()
+#    payload_pos2.grid_remove()
 
 	# add required items to grid
-    if p_count.get() == 1:
-        l2.grid(row=2, column=0, sticky=W, pady=2)
-        payload_pos1.grid(row=2, column=1, sticky=W, pady=2)
-    elif p_count.get() == 2:
-         l2.grid(row=2, column=0, sticky=W, pady=2)
-         payload_pos1.grid(row=2, column=1, sticky=W, pady=2)
-         l2_2.grid(row=3, column=0, sticky=W, pady=2)
-         payload_pos2.grid(row=3, column=1, sticky=W, pady=2)
+#    if p_count.get() == 1:
+#        l2.grid(row=2, column=0, sticky=W, pady=2)
+#        payload_pos1.grid(row=2, column=1, sticky=W, pady=2)
+#    elif p_count.get() == 2:
+#         l2.grid(row=2, column=0, sticky=W, pady=2)
+#         payload_pos1.grid(row=2, column=1, sticky=W, pady=2)
+#         l2_2.grid(row=3, column=0, sticky=W, pady=2)
+#         payload_pos2.grid(row=3, column=1, sticky=W, pady=2)
+         
+
+def add_symbols():
+    try:
+        index_start = request_input.index("sel.first")
+        index_end = request_input.index("sel.last")
+        request_input.insert(index_start, "§")
+        request_input.insert(str(float(index_end)+0.01), "§")
+    except:
+        print("ERROR: no selection!")
 
 
 
@@ -63,15 +73,15 @@ payload_pos2 = tk.Text(frame,
 
 # Labels for text boxes
 l1 = tk.Label(frame, text = "Paste full html request:")
-l2 = tk.Label(frame, text = "Identify payload: Must be exact match (regex).")
-l2_2 = tk.Label(frame, text = "Identify payload 2: Must be exact match (regex).")
+#l2 = tk.Label(frame, text = "Identify payload: Must be exact match (regex).")
+#l2_2 = tk.Label(frame, text = "Identify payload 2: Must be exact match (regex).")
 
 
 # define radio buttons to select number of payloads
-r1 = Radiobutton(frame, text="Single payload.", variable=p_count, value=1, command=func1)
+#r1 = Radiobutton(frame, text="Single payload.", variable=p_count, value=1, command=func1)
 #Radio_1.pack(side = TOP, ipady = 5) 
 #Radio_1.invoke() # selects by default
-r2 = Radiobutton(frame, text="Dual payload.", variable=p_count, value=2, command=func1)
+#r2 = Radiobutton(frame, text="Dual payload.", variable=p_count, value=2, command=func1)
 #Radio_2.pack(side = TOP, ipady = 5) 
 
 
@@ -85,26 +95,32 @@ r2 = Radiobutton(frame, text="Dual payload.", variable=p_count, value=2, command
 #l2.pack(side="bottom")
 #payload_pos1.pack(side="bottom")
 
+# button creation
+add_button = tk.Button(frame, 
+						text = "ADD §§", 
+						command = add_symbols)
 
-# Button Creation 
 attack_button = tk.Button(frame, 
 						text = "ATTACK", 
-						command = run_repeater) 
+						command = run_repeater)
 #attackButton.pack(side="bottom") 
 
 
 # insert elements into window
 
-r1.grid(row=0, column=0, sticky=W, pady=2)
-r2.grid(row=0, column=1, sticky=W, pady=2)
+#r1.grid(row=0, column=0, sticky=W, pady=2)
+#r2.grid(row=0, column=1, sticky=W, pady=2)
 l1.grid(row=1, column=0, sticky=W, pady=2)
 request_input.grid(row=1,column=1, sticky=W, pady=2)
+add_button.grid(row=4, column=0, columnspan=2)
+add_button.grid_rowconfigure(1, weight=1)
+add_button.grid_columnconfigure(1, weight=1)
 attack_button.grid(row=5, column=0, columnspan=2)
 attack_button.grid_rowconfigure(1, weight=1)
 attack_button.grid_columnconfigure(1, weight=1)
 
 # select r1 as default
-r1.invoke()
+#r1.invoke()
 
 
 
